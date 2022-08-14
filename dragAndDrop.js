0ffcf3b0358f05547e10f079ce3fc8b58f22bd04
yoavkivity <yoavkivity@gmail.com>
2022-08-14 11:06:49 +0300

    Merge versions
    Merge home version & work version into one GIT updated last version

--- dragAndDrop.js
+++ dragAndDrop.js
@@ -1,7 +1,21 @@
 const dragArea = document.querySelector('.drag-area');
 const dragText = document.querySelector('.header');
 
+ let button = document.querySelector('.button');
+ let input = document.querySelector('input');
+
 let file;
+
+button.onclick = () => {
+    input.click();
+};
+
+// when browse
+input.addEventListener('change', function() {
+    file = this.files[0];
+    dragArea.classList.add('active');
+    displayFile ();
+}) 
 
 // when file is inside the drag area
 dragArea.addEventListener('dragover', (event) => {
@@ -26,8 +40,13 @@ dragArea.addEventListener("drop",(event) => {
 
     file = event.dataTransfer.files[0];
 //      console.log(file);
+    displayFile ();
 
-let fileType = file.type;
+
+});
+
+function displayFile () {
+    let fileType = file.type;
 // console.log(fileType);
 
 let validExtensions = ['image/jpeg', 'image/jpg', 'image/png' ]
@@ -42,21 +61,22 @@ if (validExtensions.includes(fileType)) {
     };
     fileReader.readAsDataURL(file);
 } else {
-    alert('This file is not an image format')
+    alert('This file is not an image format');
+    dragArea.classList.remove('active');
 }
-
-
 //    console.log('The file is dropped in drag area')
-});
+
+};
 
 
 
 /* Missions:
 
     javaScript:
-        1. Picture size dosnt fit
-        2. Browse dos't work
-        3. Finish the video
+        Drag and Drop:
+            1. Picture size dosnt fit --------------------------------------------------- V
+            2. Browse dosn't work ------------------------------------------------------- V
+            3. Finish the video --------------------------------------------------------- V 
 
 
     css:
@@ -67,13 +87,14 @@ if (validExtensions.includes(fileType)) {
             4. change the toolBar colors (??)
 
         Home:
-            1. fix the ToolBar (too small)
-            2. center the images in the middle (with proportional from both sids)
+            1. fix the ToolBar (too small)----------------------------------------------- V
+            2. center the images in the middle (with proportional from both sids) ------- V
             3. put the images in a darker color box
             4. add more pictures
+            5. Relatives paths ---------------------------------------------------------- V
 
         contact us:
 
         about us:
 
-            */         
+            */
